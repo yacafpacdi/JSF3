5fb3590912ccc5f4d6ef478532dc4a396eb81353
cdi403 <cdi403@pltz888.AFPA-PARIS-DI>
2015-10-21 12:01:08 +0200

    ajout de l'internationalisation

--- src/main/java/utils/ChangeLocale.java
+++ src/main/java/utils/ChangeLocale.java
@@ -1,5 +1,30 @@
 package utils;
 
-public class ChangeLocale {
-    
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ChangeLocale implements Serializable {
+
+    private String locale = "fr";
+
+    public ChangeLocale() {
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public String setFrenchLocale() {
+        locale = "fr";
+        return null;
+
+    }
+
+    public String setEnglishLocale() {
+        locale = "en";
+        return null;
+    }
 }
